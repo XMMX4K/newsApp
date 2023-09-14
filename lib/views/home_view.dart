9e7e916fe8abbee710bed70742f906ddec150830
XMMX4K <mohamedelsharkawy208@gmail.com>
2023-09-14 08:41:38 +0300

    create news category item

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:newsapp/widgets/newsCategory.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -15,7 +16,7 @@ class HomeView extends StatelessWidget {
         ),
       ),
       body: Column(
-        children: [Container()],
+        children: [NewsCategory()],
       ),
     );
   }
---
+++ lib/widgets/newsCategory.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsCategory extends StatelessWidget {
+  const NewsCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
